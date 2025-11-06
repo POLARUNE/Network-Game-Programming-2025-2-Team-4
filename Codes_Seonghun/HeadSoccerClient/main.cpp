dec56c053943e6e16451fe7534968732fb9baa2c
seonghun428 <shson1211@tukorea.ac.kr>
2025-11-06 21:57:37 +0900

    소켓 통신 구조 설정
    Server는 Console을, Client는 Window를 사용하도록. 윈도우 크기는 1000 * 740.

--- Codes_Seonghun/HeadSoccerClient/main.cpp
+++ Codes_Seonghun/HeadSoccerClient/main.cpp
@@ -1,1 +1,86 @@
-#include "../Common.h"
+#include "..\Common.h"
+
+#define SERVERIP	"127.0.0.1"
+#define SERVERPORT	9000
+#define BUFSIZE		128
+
+SOCKET sock;
+char buf[BUFSIZE + 1];
+
+HINSTANCE g_hInst;
+TCHAR lpszClass[] = TEXT("HEAD SOCCER");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	if (!hPrevInstance) {
+		WndClass.cbClsExtra = 0;
+		WndClass.cbWndExtra = 0;
+		WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+		WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+		WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+		WndClass.hInstance = hInstance;
+		WndClass.lpfnWndProc = (WNDPROC)WndProc;
+		WndClass.lpszClassName = lpszClass;
+		WndClass.lpszMenuName = NULL;
+		WndClass.style = CS_HREDRAW | CS_VREDRAW;
+		RegisterClass(&WndClass);
+	}
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, 0, 0, 1000, 740, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, 0, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	int retval;
+
+	// 소켓 생성
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET) err_quit("socket()");
+
+	// connect()
+	struct sockaddr_in serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = inet_addr(SERVERIP);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = connect(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("connect()");
+
+	// 윈속 종료
+	WSACleanup();
+	return Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+
+	switch (iMessage) {
+	case WM_CREATE:
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
